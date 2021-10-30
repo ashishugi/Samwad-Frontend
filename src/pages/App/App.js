902ca62077e3a7395d66c46f96e55b9854b412fd
ashishugi <ashishkumarguptacse@gmail.com>
2021-10-30 18:32:01 +0530

    FIXED Routing with Switch

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 import { Button } from "./style";
 import Login from "../Login/Login";
@@ -10,6 +17,9 @@ import Register from "../Register/Register";
 import { authUser } from "../../actions/login";
 import _404 from "../_404/_404";
 
+const Feed = () => {
+  return <Redirect to="/feed" />;
+};
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -23,11 +33,17 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        <Route exact path="/" component={Posts}></Route>
-        <Route exact path="/signup" component={Register}></Route>
-        <Route exact path="/login" component={Login}></Route>
-        <Route exact path="/profile" component={Profile}></Route>
-        <Route exact path="**" component={_404} />
+        <Switch>
+          <Route exact path="/">
+            <Posts />
+          </Route>
+          <Route exact path="/signup" component={Register}></Route>
+          <Route exact path="/login" component={Login}></Route>
+          <Route exact path="/profile" component={Profile}></Route>
+          <Route exact path="**">
+            <_404 />
+          </Route>
+        </Switch>
       </Router>
     );
   }
